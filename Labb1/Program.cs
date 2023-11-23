cff744d2ed3af3c57ac7d8f3aa4ad8b60b89d191
Adreis100 <150135229+Adreis100@users.noreply.github.com>
2023-11-23 16:45:15 +0100

    Add files via upload

---
+++ Labb1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace Labb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ange namn 1");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("ange ålder 1");
+            int age1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ange namn 2");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("ange ålder 2");
+            int age2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ange namn 3");
+            string name3 = Console.ReadLine();
+            Console.WriteLine("ange ålder 3");
+            int age3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ange namn 4");
+            string name4 = Console.ReadLine();
+            Console.WriteLine("ange ålder 4");
+            int age4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(name1 + " är " + age1 + " år gammal");
+            Console.WriteLine(name2 + " är " + age2 + " år gammal");
+            Console.WriteLine(name3 + " är " + age3 + " år gammal");
+            Console.WriteLine(name4 + " är " + age4 + " år gammal");
+
+            double totalage = age1 + age2 + age3 + age4;
+            double median = totalage / 4;
+            string dec = median.ToString("0.00"); 
+            Console.WriteLine("totalålder " + totalage);
+            Console.WriteLine("medelålder " + dec);
+
+            
+
+           
+        }
+    }
+}
+
+ 
